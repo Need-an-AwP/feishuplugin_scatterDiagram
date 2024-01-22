bbf8f9f23e76557aad21b51582f13fd6dcebb635
Need-an-AwP <1790414525klz@gmail.com>
2024-01-22 20:59:52 +0800

    changed & complete a lot things, no clustering method added

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
 import './App.css';
 import { useEffect, useState, useRef, SetStateAction } from 'react';
-import { bitable, FieldType, UIBuilder } from "@lark-base-open/js-sdk";
+import { bitable, FieldType } from "@lark-base-open/js-sdk";
 import { useTranslation } from 'react-i18next';
 import { UseTranslationResponse } from 'react-i18next';
 import './i18n';
 import { G2, Datum, Scatter, Mix } from "@antv/g2plot";
 import { InputNumber, Progress, Space, Radio, message, Switch, Select, Button, Flex, Alert, ColorPicker, Col, Divider, Row, SelectProps, Typography } from 'antd';
-import type { RadioChangeEvent } from 'antd';
+import type { RadioChangeEvent, ColorPickerProps } from 'antd';
 import html2canvas from 'html2canvas';
 import { float } from 'html2canvas/dist/types/css/property-descriptors/float';
 import { kmeans } from 'ml-kmeans';
 import { DBSCAN } from 'density-clustering';
 import { isString } from 'antd/es/button';
+import moment from 'moment';
 
 
 export default function App() {
@@ -23,6 +24,8 @@ export default function App() {
     const [percent, setPercent] = useState<number>(0);
     const isNextButtonDisabled = !(selectedValues[0]);
     const [regressionSelect, setregressionSelect] = useState('none');
+    const [regressionParams, setregressionParams] = useState([5, 60])
+    const [regressionLineColor, setregressionLineColor] = useState('#7f7f7f')
     const [clusteringSelect, setclusteringSelect] = useState('none');
     const [kmeansParams, setkmeansParams] = useState([3, 100]);
     const [dbscanParams, setdbscanParams] = useState([5, 2])
@@ -49,12 +52,15 @@ export default function App() {
 
     }, []);
 
+    function findDateByIndex(indexToFind, odata) {
+        const foundItem = odata.find(item => item.index.toString() === indexToFind);
+        return foundItem ? foundItem.date : null;
+    }
+
     const chartComponent = async (
         data: any[],
         xField = 'x',
         yField = 'y',
-        pcolorField = '',
-
     ) => {
         const oldChart = chartContainerRef.current.chart;
         if (oldChart) {
@@ -88,10 +94,10 @@ export default function App() {
 
         console.log(xField);
         console.log(yField);
-        console.log(pcolorField);
         console.log(data)
         const mixPlot = new Mix(chartContainerRef.current, {
             tooltip: { shared: true },
+            appendPadding: 3,
             syncViewPadding: true,
             plots: [
                 {
@@ -101,10 +107,32 @@ export default function App() {
                         xField: xField,
                         yField: yField,
                         colorField: 'cluster',
-                        regressionLine: { type: 'log' },
+                        regressionLine: {
+                            top: true,
+                            type: regressionSelect,
+                            style: {
+                                lineWidth: regressionParams[0],
+                                stroke: regressionLineColor,
+                                strokeOpacity: regressionParams[1] / 100,
+                            },
+                        },
+                        pointStyle: { strokeOpacity: 1 },
+                        yAxis: { grid: { line: {} } },
+                        xAxis: {
+                            grid: null,
+                            label: {
+                                formatter: (datum, index) => {
+                                    if (data[0].parsedDate != null) {
+                                        const a = findDateByIndex(datum, data);
+                                        return a;
+                                    }else {
+                                        return datum;
+                                    }
+                                },
+                            },
+                        },
                     },
                 },
-
             ]
         })
         mixPlot.render();
@@ -119,6 +147,12 @@ export default function App() {
 
     const handleRegressionSelect = (value: string | undefined) => {
         setregressionSelect(value);
+    };
+
+    const handleRegressionParams = (index: number, value: number) => {
+        const newRP = [...regressionParams];
+        newRP[index] = value;
+        setregressionParams(newRP);
     };
 
     const handleclusteringSelect = (value: string | undefined) => {
@@ -143,8 +177,8 @@ export default function App() {
                 return (
                     <>
                         <Row gutter={16}>
-                            <Col span={12}>聚类数量</Col>
-                            <Col span={12}>
+                            <Col span={13} style={{ display: 'flex', alignItems: 'center', fontWeight: 'lighter' }}>聚类数量</Col>
+                            <Col span={9}>
                                 <InputNumber
                                     min={1}
                                     size='small'
@@ -154,8 +188,8 @@ export default function App() {
                             </Col>
                         </Row>
                         <Row gutter={16} >
-                            <Col span={11}>最大迭代数</Col>
-                            <Col span={12}>
+                            <Col span={12} style={{ display: 'flex', alignItems: 'center', fontWeight: 'lighter' }}>最大迭代数</Col>
+                            <Col span={9}>
                                 <InputNumber
                                     size='small'
                                     defaultValue={100}
@@ -164,7 +198,7 @@ export default function App() {
                             </Col>
                         </Row>
                         <Row gutter={16} >
-                            <Col span={10}>初始化方法</Col>
+                            <Col span={11} style={{ display: 'flex', alignItems: 'center', fontWeight: 'lighter' }}>初始化方法</Col>
                             <Col span={9}>
                                 <Radio.Group defaultValue={'kmeans++'}>
                                     <Space direction="vertical">
@@ -182,8 +216,8 @@ export default function App() {
                 return (
                     <>
                         <Row gutter={16}>
-                            <Col span={12}>邻域半径</Col>
-                            <Col span={12}>
+                            <Col span={13} style={{ display: 'flex', alignItems: 'center', fontWeight: 'lighter' }}>邻域半径</Col>
+                            <Col span={9}>
                                 <InputNumber
                                     min={1}
                                     size='small'
@@ -193,8 +227,8 @@ export default function App() {
                             </Col>
                         </Row>
                         <Row gutter={16} >
-                            <Col span={12}>最小点数</Col>
-                            <Col span={12}>
+                            <Col span={13} style={{ display: 'flex', alignItems: 'center', fontWeight: 'lighter' }}>最小点数</Col>
+                            <Col span={9}>
                                 <InputNumber
                                     size='small'
                                     defaultValue={2}
@@ -208,6 +242,51 @@ export default function App() {
             default:
                 return null;
         }
+    };
+
+    const renderRegressionOptions = () => {
+        if (regressionSelect != 'none') {
+            return (
+                <>
+                    <Row gutter={16}>
+                        <Col span={12} style={{ display: 'flex', alignItems: 'center', fontWeight: 'lighter' }}>回归线粗细</Col>
+                        <Col span={12}>
+                            <InputNumber
+                                min={1}
+                                size='small'
+                                defaultValue={regressionParams[0]}
+                                onChange={(value) => handleRegressionParams(0, value)}
+                            />
+                        </Col>
+                    </Row>
+                    <Row gutter={16}>
+                        <Col span={17} style={{ display: 'flex', alignItems: 'center', fontWeight: 'lighter' }}>回归线颜色</Col>
+                        <Col span={5}>
+                            <ColorPicker
+                                value={regressionLineColor}
+                                disabledAlpha
+                                onChangeComplete={(color) => {
+                                    setregressionLineColor(color.toHexString());
+                                }}
+                            />
+                        </Col>
+                    </Row>
+                    <Row gutter={16}>
+                        <Col span={12} style={{ display: 'flex', alignItems: 'center', fontWeight: 'lighter' }}>回归线透明度</Col>
+                        <Col span={5}>
+                            <InputNumber
+                                min={1}
+                                max={99}
+                                size='small'
+                                defaultValue={regressionParams[1]}
+                                onChange={(value) => handleRegressionParams(1, value)}
+                            />
+                        </Col>
+                    </Row>
+                </>
+            );
+        }
+
     };
 
     const handleNextButtonClick = async () => {
@@ -226,7 +305,7 @@ export default function App() {
             message.error('绘图数据错误', 2);
             return
         } else { }
-
+        //get data from feishu
         const origin_data = [];
         for (let i of recordIdList) {
             const recordData = {};
@@ -238,64 +317,63 @@ export default function App() {
             setPercent(parseFloat(((recordIdList.indexOf(i) + 1) / recordIdList.length * 100).toFixed(1)))
             //console.log(parseFloat(((recordIdList.indexOf(i) + 1) / recordIdList.length * 100).toFixed(1)))
         }
-
-        origin_data.map((item, index) => {
+        //handle if data contains date
+        origin_data.forEach((item, index) => {
             for (let key in item) {
-                if (!isNaN(parseFloat(item[key])) && !item[key].includes('-')) {
-                    item[key] = parseFloat(item[key])
+                const value = item[key];
+
+                if (!isNaN(parseFloat(value)) && !value.includes('-')) {
+                    item[key] = parseFloat(value);
+                } else if (moment(value, moment.ISO_8601, true).isValid()) {
+                    item['parsedDate'] = moment(value);
                 }
             }
-            item['index'] = index + 1;
-        })
-        const data = origin_data;
+        });
+        origin_data.sort((a, b) => {
+            const datetimeA = a['parsedDate'];
+            const datetimeB = b['parsedDate'];
+
+            if (datetimeA && datetimeB) {
+                return datetimeA - datetimeB;
+            }
+            return 0;
+        });
+        origin_data.forEach((item, index) => { item['index'] = index + 1; })
+
+
         const xField = selectedValues[1];
         const yField = selectedValues[0];
+        //process cluster data
+        let loc_data = [];
+        if (isString(origin_data[0][xField])) {
+            for (let i of origin_data) {
+                let a = [];
+                a.push(i['index']);
+                a.push(i[yField]);
+                loc_data.push(a);
+            }
+        } if (isString(origin_data[0][yField])) {
+            for (let i of origin_data) {
+                let a = [];
+                a.push(i[xField]);
+                a.push(i['index']);
+                loc_data.push(a);
+            }
+        }
         // kmeans clustering
         if (clusteringSelect == 'kmeans') {
-            let loc_data = [];
-            if (isString(data[0][xField])) {
-                for (let i of data) {
-                    let a = [];
-                    a.push(i['index']);
-                    a.push(i[yField]);
-                    loc_data.push(a);
-                }
-            } else {
-                for (let i of data) {
-                    let a = [];
-                    a.push(i[xField]);
-                    a.push(i[yField]);
-                    loc_data.push(a);
-                }
-            }
             const { clusters, centroids } = kmeans(
                 loc_data,
                 kmeansParams[0],
                 { initialization: 'kmeans++', maxIterations: kmeansParams[1] }
             )
-            for (let i = 0; i < data.length; i++) {
-                data[i].cluster = clusters[i];
+            for (let i = 0; i < origin_data.length; i++) {
+                origin_data[i].cluster = clusters[i];
             }
 
         }
         //DBSCAN clustering
         if (clusteringSelect == 'dbscan') {
-            let loc_data = [];
-            if (isString(data[0][xField])) {
-                for (let i of data) {
-                    let a = [];
-                    a.push(i['index']);
-                    a.push(i[yField]);
-                    loc_data.push(a);
-                }
-            } else {
-                for (let i of data) {
-                    let a = [];
-                    a.push(i[xField]);
-                    a.push(i[yField]);
-                    loc_data.push(a);
-                }
-            }
             loc_data = loc_data.map(point => point.map(value => Number.isFinite(value) ? value : 0));
             let dbscan = new DBSCAN;
             const clusters = dbscan.run(loc_data, dbscanParams[0], dbscanParams[1]);
@@ -306,8 +384,8 @@ export default function App() {
                     pointToCluster[pointIndex] = index;
                 });
             });
-            for (let i = 0; i < data.length; i++) {
-                data[i].cluster = pointToCluster[i];
+            for (let i = 0; i < origin_data.length; i++) {
+                origin_data[i].cluster = pointToCluster[i];
             }
         }
 
@@ -349,7 +427,8 @@ export default function App() {
                 )}
             </Space>
             <Divider />
-            <Space wrap style={{ width: '100%', marginBottom: '20px' }} direction="horizontal">
+            <Space wrap style={{ width: '100%', marginBottom: '10px' }} direction="horizontal">
+
                 <span style={{ fontSize: '15px', marginRight: '15px' }}>绘制回归线</span>
                 <Select
                     style={{ width: 150, marginRight: '10px' }}
@@ -366,6 +445,10 @@ export default function App() {
                     onChange={(value) => handleRegressionSelect(value)}
                     defaultValue='none'
                 />
+                {renderRegressionOptions()}
+            </Space>
+
+            <Space wrap style={{ width: '100%', marginBottom: '20px' }} direction="horizontal">
                 <span style={{ fontSize: '15px' }}>选择聚类算法</span>
                 <Select
                     style={{ width: 150, marginRight: '10px' }}
@@ -399,6 +482,7 @@ export default function App() {
                 type="default"
                 block
                 style={{ marginBottom: '20px' }}
+
             >
                 将聚类结果添加到表格
             </Button>
